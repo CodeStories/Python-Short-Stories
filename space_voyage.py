bcffdb8f1fff34bae8b98bc728c1fb8080fe6d2a
José Reyna <jose.reyna.rb@gmail.com>
2014-07-15 19:28:43 -0430

    Some tabs in space_voyage turned to spaces. Shameful stuff from LibreOffice.

--- space_voyage.py
+++ space_voyage.py
@@ -7,13 +7,13 @@ import itertools
 
 def space_voyage(spaceport, ship_ids, destination):
    	if world.exists(destination):
-		params = []
-		for id in ship_ids:
-			if spaceport.queryHangar(id).isReady():
-				params.append(id, destination)
-		if len(params) > 0:
-			return itertools.starmap(spaceport.launch, params)
-		else:
-			raise spaceport.NoShipsReadyException()
-	else:
-		raise world.DestinationException()
+        params = []
+        for id in ship_ids:
+            if spaceport.queryHangar(id).isReady():
+                params.append(id, destination)
+        if len(params) > 0:
+            return itertools.starmap(spaceport.launch, params)
+        else:
+            raise spaceport.NoShipsReadyException()
+    else:
+        raise world.DestinationException()
